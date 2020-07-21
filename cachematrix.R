43e3c613b9d1f6a6c4629c79d5a105ade6a10f9a
tc178 <64407571+tc178@users.noreply.github.com>
2020-07-21 10:40:31 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The purpose of this function is to create a special matrix that can 
+## cache its inverse
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+ setwd('C:/Users/tomcr/Documents/Coursera-R')
 
+makeCacheMatrix <- function(x = matrix()) {
+inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) inv <<- inverse
+    getinverse <- function() inv
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Now this function computes the inverse of the above function
+## The purpose of cacheSolve is to retrieve the inverse of the Cache 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        cacheSolve <- function(x, ...) {
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinverse(inv)
+    inv
 }
